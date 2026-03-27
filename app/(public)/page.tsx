28f215a8fc6bb77deeeb8ff1b7ba60e23ce37c7e
elguapo87 <pglogovac@gmail.com>
2026-03-27 16:12:14 +0100

    Created Hero component

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/home/Banner";
+import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Hero />
     </div>
   );
 }
