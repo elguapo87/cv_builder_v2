4e997c3beda814bfdfeeb400e78292f5603aae80
elguapo87 <pglogovac@gmail.com>
2026-04-01 20:47:47 +0200

    Created genToken utility function

---
+++ utils/genToken.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const genToken = (userId: string) => {
+    const jwtSecret = process.env.JWT_SECRET!;
+    if (!jwtSecret) {
+        throw new Error("JWT_SECRET is not defined in .env")
+    }
+
+    const token = jwt.sign({ userId }, jwtSecret, { expiresIn: "7d" });
+
+    return token;
+};
