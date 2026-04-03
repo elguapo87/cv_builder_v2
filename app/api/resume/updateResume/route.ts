1117d5f9d396ce4ba98a9a3e9cf4358f7f0d6d9a
elguapo87 <pglogovac@gmail.com>
2026-04-03 02:50:33 +0200

    Created updateResume API

---
+++ app/api/resume/updateResume/route.ts
@@ -0,0 +1,58 @@
+import DBConnection from "@/config/db";
+import imageKit from "@/config/imageKit";
+import protectUser from "@/middleware/protectUser";
+import resumeModel from "@/models/resumeModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        await DBConnection();
+
+        const userId = await protectUser(req);
+
+        const form = await req.formData();
+        const resumeId = form.get("resumeId") as string;
+        const resumeData = form.get("resumeData") as string;
+        const removeBackground = form.get("removeBackground") === "true";
+        const image = form.get("image") as File | null;
+
+        let imageUrl = "";
+
+        if (image && image.size > 0) {
+            const bytes = await image.arrayBuffer();
+            const buffer = Buffer.from(bytes);
+
+            const uploadRes = await imageKit.upload({
+                file: buffer,
+                fileName: `profile_${userId}_${Date.now()}_${image.name}`,
+                folder: "/cv_builder"
+            });
+
+            const transformation = removeBackground
+                ? "e-bgremove,w-300,h-300,fo-face,z-0.75"
+                : "w-300,h-300,fo-face,z-0.75";
+
+            imageUrl = imageKit.url({
+                path: uploadRes.filePath,
+                transformation: [{ raw: transformation }]
+            });
+        }
+
+        const parsedResumeData = JSON.parse(resumeData);
+        if (imageUrl) {
+            parsedResumeData.personal_info.image;
+        }
+
+        const resume = await resumeModel.findByIdAndUpdate(
+            { _id: resumeId, userId },
+            { $set: parsedResumeData },
+            { new: true }
+        );
+
+        return NextResponse.json({ success: true, message: "Saved", resume }, { status: 200 });
+
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        return NextResponse.json({ success: false, message: errMessage }, { status: 500 });
+    }
+}
